eb652e72b38dbc97f8f76347dcbd24b2c9495e26
Canera <yaniscanera30@gmail.com>
2025-01-13 11:19:41 +0100

    Initial commit

---
+++ src/CMakeNinja.cpp
@@ -0,0 +1,11 @@
+﻿// CMakeNinja.cpp : définit le point d'entrée de l'application.
+//
+
+#include "../include/CMakeNinja.h"
+
+int main()
+{
+	std::cout << "Hello CMake." << std::endl;
+	std::getchar();
+	return 0;
+}
